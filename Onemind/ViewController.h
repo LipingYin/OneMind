b3eb29343ef7fbab932e040de59c68f25bb80318
Lipingyin <lipingyin1991@163.com>
2014-06-30 19:56:11 +0800

    Initial Commit

---
+++ Onemind/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Onemind
+//
+//  Created by yinliping on 14-6-30.
+//  Copyright (c) 2014年 LipingYin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
